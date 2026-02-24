a6508df4f8070b518547cbef8fc9cbb6385c7db4
Xaolin_do_38Malado <guilhermewandson737@gmail.com>
2026-02-24 20:07:46 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import com.nelioalves.workshopmongo.resources.UserResource;
@@ -18,22 +19,21 @@ import com.nelioalves.workshopmongo.resources.UserResource;
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
-    private final UserResource userResource;
+	private final UserResource userResource;
 
 	@Autowired
 	private UserRepository userRopository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
-    Instantiation(UserResource userResource) {
-        this.userResource = userResource;
-    }
+	Instantiation(UserResource userResource) {
+		this.userResource = userResource;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
@@ -43,14 +43,22 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRopository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));	
 		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3)); 
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-	
+
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRopository.save(maria);
 	}
